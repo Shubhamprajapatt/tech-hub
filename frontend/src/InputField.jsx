dd3d9c74867d67d687b78c1c32cc2b88a93a4582
shubham <prajapatshubham989@gmail.com>
2024-04-09 08:59:31 +0530

    backend API Calling

--- frontend/src/InputField.jsx
+++ frontend/src/InputField.jsx
@@ -3,10 +3,11 @@ import React from "react";
 export default function InputField(props){
     return(
         <div className='p-3'>
-        <label>
+            <span>*</span>
+        <label className="text-xl ml-2" style={{color:"rgba(0, 0, 0, 0.88)"}}>
             {props.label}
         </label>
-        <input type="text" placeholder={props.placeholder} className='p-2 rounded ml-4'/>
+        <input type="text" placeholder={props.placeholder} className='p-2 rounded ml-4' onChange={props.onChange}/>
       </div>
     )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,97 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import { Checkbox, Form, Input } from "antd";
 import Button from "../components/Button";
-import Link from "antd/es/typography/Link";
-import {useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
-  const navigate=useNavigate();
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("asdasd");
+  const onFinish = async (values) => {
+    console.log("Success:", values.username, values.password);
+    //api calling
+    axios
+      .post("http://localhost:8080/login", {
+        email: values.username,
+        password: values.password,
+      })
+      .then((res) => {
+        console.log("backend response", res);
+        if (res.data.status == 1) {
+          alert("success");
+        } else {
+          alert(`${res.data.message}`);
+        }
+      })
+      .catch((err) => {
+        console.log("backend error", err);
+        alert(`${err.message}`);
+      });
   };
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
-  const handleRegister=()=>{
+  const handleRegister = () => {
     navigate("/register");
-  }
+  };
+  const handlePassword = (e) => {
+    console.log("value:", e.target.value);
+  };
   return (
     <>
-    <div className="content">
-      <div>
-      <h1>Login</h1>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        style={{
-          maxWidth: 600,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your username!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+      <div className="content">
+        <center>
+          <div>
+            <h1 className="mb-4">Login</h1>
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              style={{
+                maxWidth: 600,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                ]}
+              >
+                <Input
+                  value={email}
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="enter username"
+                />
+              </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input
+                  placeholder="enter password"
+                  value={password}
+                  name="password"
+                  onChange={(e) => handlePassword(e)}
+                />
+              </Form.Item>
 
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
 
-        <Form.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-      <div>
-      <span>Don't have an Account</span><p className="text-white text-xl cursor-pointer" onClick={handleRegister}>Register</p>
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <div>
+                  <span>Don't have an Account ? 
+                  <Link to="/register"> Register</Link>
+                  </span>
+                </div>
+
+                <Button type="primary" htmlType="submit" name="Submit" />
+              </Form.Item>
+            </Form>
+          </div>
+        </center>
       </div>
-
-          <Button type="primary" htmlType="submit" name="Submit"/>
-
-        </Form.Item>
-
-      </Form>
-      </div>
-    </div>
     </>
   );
 }
